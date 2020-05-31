92c07565748b0cbd9c91146a83c45405d7900731
valvaro-1099 <valvaro@deakin.edu.au>
2020-05-31 23:20:40 +1000

    Work on joystick
    Worked on joystick for the player movement

--- Tidying Up/GameScene.swift
+++ Tidying Up/GameScene.swift
@@ -16,52 +16,77 @@ class GameScene: SKScene {
 //      array that will hold the different type of animation
     var idlearray = [SKTexture]()
     var runningarray = [SKTexture()]
-    var moving = true
-    
+   var Base = SKSpriteNode(imageNamed: "base")
+    var joystick = SKSpriteNode(imageNamed: "jostick")
+//    check if joystick is touch
+
+
     override func didMove(to view: SKView) {
         
         self.scaleMode = .fill
-//        for loop to load all the assets for idle animation to the idlearray
-        for i in 1..<16
+        for i in 1..<10
         {
-            let textturename = "Idle\(i)"
+            let textturename2 = "Run (\(i))"
+            let textturename = "Idle (\(i))"
             idlearray.append(SKTexture(imageNamed: textturename))
+            runningarray.append(SKTexture(imageNamed: textturename2))
         }
-        for i in 1..<20
-        {
-            let texturename = "Run (\(i))"
-            runningarray.append(SKTexture(imageNamed: texturename))
-        }
-
-       
-    
-    }
-//    drag type of touch
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        removeAllActions()
+//        width: 194.09, height: 185.34)
+        Base.size = CGSize(width: 194, height: 184)
+        Base.position = CGPoint(x:-470.162 , y: -354.178)
+        Base.addChild(joystick)
+        joystick.position = Base.position;
+        joystick.size = CGSize(width: 120, height: 140)
         
+       
+    }
+////    drag type of touch
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//
+//
+//        for touch in touches
+//        {
+//
+//            player.run(SKAction.animate(with: runningarray, timePerFrame: 0.1, resize: false, restore: true))
+//             let touchlocation = touch.location(in: self)
+//            player.position = touchlocation;
+//        }
+//    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let touch = touches.first!
+//        let location = touch.location(in: self)
+//        player.run(SKAction.move(to: location, duration: 2))
+        
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches
         {
-
-            player.run(SKAction.animate(with: runningarray, timePerFrame: 0.1))
-             let touchlocation = touch.location(in: self)
-            player.position = touchlocation;
+            let location = touch.location(in: self)
+            let vector = CGVector(dx: location.x - Base.position.x, dy: location.y - Base.position.y)
+            let angle = atan2(vector.dy, vector.dx)
+//            let deg = angle * CGFloat(180 / M_PI);
             
+            let lenght:CGFloat = Base.frame.size.height / 2 - 20
+                  let xDist: CGFloat = sin(angle - 1.57079633) * lenght
+                  let yDist: CGFloat = cos(angle - 1.57079633) * lenght
+
+            
+            joystick.position = CGPoint(x: Base.position.x - xDist, y: Base.position.y + yDist)
+
+            if (Base.frame.contains(location))
+            {
+
+                       joystick.position = location
+            }else
+            {
+
+                joystick.position = CGPoint(x: Base.position.x - xDist, y: Base.position.y + yDist)
+
+            }
+
             
             
         }
-    }
-//    for when the player is idle
-    func playeridle()
-    {
-        while(!moving)
-        {
-            player.run(SKAction.animate(with: idlearray, timePerFrame: 0.1))
-        }
-    
-//        player.run(SKAction.repeatForever(SKAction.animate(with: idlearray, timePerFrame: 0.1)))
-//        player.run(SKAction.repeatForever(SKAction.animate(with: <#T##[SKTexture]#>, timePerFrame: <#T##TimeInterval#>))
-
     }
 
     
